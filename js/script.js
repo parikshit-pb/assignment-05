eea256c590162165ac878fa151fbc2a17801ffde
parikshit-pb <papon46857@gmail.com>
2025-12-07 01:55:00 +0600

    added mobile responsive

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
+const copyButtons = document.querySelectorAll(".copy-btn")
+
 const modal = document.getElementById('call-modal');
+
 const modalTitle = document.getElementById('modal-title');
 const modalMsg = document.getElementById('modal-msg');
 const closeModal = document.getElementById('close-modal');
+const copyCount = document.getElementById("copy-count");
 
-
+let totalCopy = 0;
+let pendingTitle = "";
 let pendingName = "";
 let pendingNumber ="";
 
@@ -20,6 +25,23 @@ hearts.forEach(heart => {
         heart.classList.toggle('fa-regular')
     });
 });
+// copy btn
+copyButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const number = button.getAttribute("data-number");
+        const title = button.getAttribute("data-title");
+
+        navigator.clipboard.writeText(number).then(() => {
+            alert(`Copy Done- ${title} (${number})`);
+
+            totalCopy++;
+            copyCount.textContent = totalCopy;
+        })
+        .catch(err => {
+            console.error("copy failed:", err);
+        });
+    });
+});
 
 // coin add
 
@@ -31,35 +53,40 @@ function updateCoin () {
 const callButtons = document.querySelectorAll('.call-btn')
 const historyList = document.getElementById('history-list')
 
-callButtons.forEach(button => {
+    callButtons.forEach(button => {
     button.addEventListener('click', function () {
 
         const title = this.getAttribute("data-title");
         const name = this.getAttribute("data-name");
         const number = this.getAttribute('data-number')
 
+        const now = new Date();
+         timeString = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
+
     if (coins < 20) {
        modalTitle.textContent = "Not Enough Coins"
        modalMsg.textContent = "You need minimum 20 coins to make a call."
        modal.classList.remove("hidden");
+       modal.classList.add("flex")
        return;
         
     }
     // temp
     pendingName = name;
     pendingNumber = number;
+    pendingTitle = title;
 
     // open model
-     modalTitle.textContent = title;
-    modalMsg.textContent = `Calling ${name} at ${number}`;
+     modalTitle.textContent = `${title}`;modalMsg.innerHTML = 
+     `Calling ${name} ${number}`;
         modal.classList.remove("hidden");
-    
+        modal.classList.add("flex");
        
     });
 });
 
 // close modal
-closeModal.addEventListener("click", () => {
+    closeModal.addEventListener("click", () => {
     if (pendingName !== "" && pendingNumber !== "") {
         // coin
 
@@ -69,15 +96,24 @@ closeModal.addEventListener("click", () => {
     // history
     const li = document.createElement("li");
     li.className = "p-2 bg-gray-100 rounded";
-    li.textContent = `${pendingName} - ${pendingNumber}`;
+    li.innerHTML = `
+    <div class= "flex justify-between">
+        <span class="font-bold">${pendingTitle}</span>
+        <span class="text-xs text-gray-500">${timeString}</span>
+    </div>
+    <p class="text-gray-600 text-sm">${pendingNumber}</p>
+    `;
     historyList.appendChild(li);
 
     pendingName = "";
     pendingNumber ="";
+    pendingTitle ="";
 
     }
     modal.classList.add("hidden");
-})
+    modal.classList.remove("flex");
+
+});
 
  
 
